9a8b37c8b9e8c677735f6a6ffb56001be8da396e
leikareipa <drilledicecream@gmail.com>
2020-10-20 15:43:04 +0300

    command-line: Flesh out range check for INPUT_CHANNEL_IDX

--- src/common/command_line/command_line.cpp
+++ src/common/command_line/command_line.cpp
@@ -31,14 +31,40 @@ static std::string FILTERS_FILE_NAME = "";
 
 bool kcom_parse_command_line(const int argc, char *const argv[])
 {
+    const char parseFailMsg[] = "VCS has to exit because it found unexpected values "
+                                "while parsing the command line. More information "
+                                "will have been printed into the console. If a "
+                                "console window was not already open, run VCS "
+                                "again from the command line.";
+
     int c = 0;
     while ((c = getopt(argc, argv, "i:m:v:a:f:")) != -1)
     {
         switch (c)
         {
-            case 'i':   // Capture input channel (>0).
+            case 'i':   // Capture input channel.
             {
                 INPUT_CHANNEL_IDX = strtol(optarg, NULL, 10);
+                
+                // Validate.
+                {
+                    const unsigned minChannelIdx = 1;    // Values must be 1-indexed.
+                    const unsigned maxChannelIdx = 8192; // Sanity check.
+
+                    if ((INPUT_CHANNEL_IDX < minChannelIdx) ||
+                        (INPUT_CHANNEL_IDX > maxChannelIdx))
+                    {
+                        NBENE(("Input channel index out of bounds. Expected range: %u-%u.",
+                               minChannelIdx, maxChannelIdx));
+
+                        kd_show_headless_error_message("", parseFailMsg);
+
+                        return false;
+                    }
+
+                    // VCS expects the channel index to be 0-indexed, so let's convert.
+                    INPUT_CHANNEL_IDX--;
+                }
 
                 break;
             }
@@ -63,25 +89,6 @@ bool kcom_parse_command_line(const int argc, char *const argv[])
             }
         }
     }
-
-    // Expect to get the input channel as 1-indexed on the command line.
-    if (INPUT_CHANNEL_IDX == 0)
-    {
-        NBENE(("Detected an invalid input channel (0). The first capture channel "
-               "is expected to be given as 1."));
-
-        kd_show_headless_error_message("",
-                                       "VCS has to exit because it found unexpected data while "
-                                       "parsing the command line. More information "
-                                       "will have been printed into the console. If a "
-                                       "console window was not already open, run VCS "
-                                       "again from the command line.");
-
-        return false;
-    }
-
-    // Convert to 0-indexed.
-    INPUT_CHANNEL_IDX--;
 
     return true;
 }
