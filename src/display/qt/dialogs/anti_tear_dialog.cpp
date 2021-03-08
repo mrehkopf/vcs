8278dd9fbc213470f6fc035f5e0ce3db9ee426db
leikareipa <drilledicecream@gmail.com>
2021-03-08 19:44:27 +0200

    gui/anti-tear-dialog: Adopt ParameterGrid subclass
    This commit implements the GUI controls for adjusting the anti-tear
    parameters using ParameterGrid, replacing the previous custom
    implementation.

--- src/display/qt/dialogs/anti_tear_dialog.cpp
+++ src/display/qt/dialogs/anti_tear_dialog.cpp
@@ -8,10 +8,14 @@
  */
 
 #include <QMenuBar>
+#include "display/qt/subclasses/QGroupBox_parameter_grid.h"
 #include "display/qt/dialogs/anti_tear_dialog.h"
 #include "display/qt/persistent_settings.h"
 #include "display/qt/utility.h"
+#include "common/propagate/app_events.h"
 #include "filter/anti_tear.h"
+#include "capture/capture.h"
+#include "capture/capture_api.h"
 #include "display/display.h"
 #include "common/globals.h"
 #include "ui_anti_tear_dialog.h"
@@ -44,7 +48,7 @@ AntiTearDialog::AntiTearDialog(QWidget *parent) :
                 enable->setChecked(true);
                 update_visualization_options();
 
-                ui->groupBox_parameters->setEnabled(true);
+                ui->parameterGrid_parameters->setEnabled(true);
                 ui->groupBox_visualization->setEnabled(true);
             });
 
@@ -53,7 +57,7 @@ AntiTearDialog::AntiTearDialog(QWidget *parent) :
                 enable->setChecked(false);
                 update_visualization_options();
 
-                ui->groupBox_parameters->setEnabled(false);
+                ui->parameterGrid_parameters->setEnabled(false);
                 ui->groupBox_visualization->setEnabled(false);
             });
 
@@ -71,64 +75,49 @@ AntiTearDialog::AntiTearDialog(QWidget *parent) :
         this->layout()->setMenuBar(this->menubar);
     }
 
+    // Set GUI controls to their initial values.
+    {
+        // Parameters.
+        {
+            const auto defaults = kat_default_settings();
+
+            ui->parameterGrid_parameters->add_parameter("Scan start", defaults.rangeUp, 0, 640);
+            ui->parameterGrid_parameters->add_parameter("Scan end", defaults.rangeDown, 0, 640);
+            ui->parameterGrid_parameters->add_spacer();
+            ui->parameterGrid_parameters->add_parameter("Threshold", defaults.threshold, 0, 255);
+            ui->parameterGrid_parameters->add_parameter("Window size", defaults.windowLen, 0, 640);
+            ui->parameterGrid_parameters->add_parameter("Step size", defaults.stepSize, 0, 640);
+            ui->parameterGrid_parameters->add_parameter("Matches req'd", defaults.matchesReqd, 0, 99);
+        }
+    }
+
+    // Subscribe to app events.
+    {
+        ke_events().capture.newVideoMode.subscribe([this]
+        {
+            const auto screenHeight = kc_capture_api().get_resolution().h;
+
+            ui->parameterGrid_parameters->set_maximum_value("Scan start", screenHeight);
+            ui->parameterGrid_parameters->set_maximum_value("Scan end", screenHeight);
+            ui->parameterGrid_parameters->set_maximum_value("Window size", screenHeight);
+            ui->parameterGrid_parameters->set_maximum_value("Step size", screenHeight);
+        });
+    }
+
     // Connect the GUI controls to consequences for changing their values.
     {
-        // Wire up the buttons for resetting individual video parameters.
-        for (int i = 0; i < ui->groupBox_parameters->layout()->count(); i++)
+        connect(ui->parameterGrid_parameters, &ParameterGrid::parameter_value_changed, this, [this](const QString &parameterName)
         {
-            QWidget *const button = ui->groupBox_parameters->layout()->itemAt(i)->widget();
+            const auto newValue = ui->parameterGrid_parameters->value(parameterName);
 
-            // The button name is expected to identify its video parameter affiliation. E.g.
-            // a button called "pushButton_reset_value_horPos" is for resetting the horizontal
-            // video position.
-            if (!button->objectName().startsWith("pushButton_reset_value_"))
-            {
-                continue;
-            }
-
-            connect((QPushButton*)button, &QPushButton::clicked, this, [this, button]
-            {
-                const QString paramId = button->objectName().replace("pushButton_reset_value_", "");
-                const auto defaults = kat_default_settings();
-
-                kat_set_buffer_updates_disabled(true);
-
-                if (paramId == "scanStart") ui->spinBox_rangeUp->setValue(defaults.rangeUp);
-                else if (paramId == "scanEnd") ui->spinBox_rangeDown->setValue(defaults.rangeDown);
-                else if (paramId == "threshold") ui->spinBox_threshold->setValue(defaults.threshold);
-                else if (paramId == "windowSize") ui->spinBox_domainSize->setValue(defaults.windowLen);
-                else if (paramId == "stepSize") ui->spinBox_stepSize->setValue(defaults.stepSize);
-                else if (paramId == "matchesReqd") ui->spinBox_matchesReqd->setValue(defaults.matchesReqd);
-                else k_assert(0, "Unknown parameter identifier");
-
-                kat_set_buffer_updates_disabled(false);
-            });
-        }
-
-        // The valueChanged() signal is overloaded for int and QString, and we
-        // have to choose one. I'm using Qt 5.5, but you may have better ways
-        // of doing this in later versions.
-        #define OVERLOAD_INT static_cast<void (QSpinBox::*)(int)>
-
-        connect(ui->spinBox_rangeUp, OVERLOAD_INT(&QSpinBox::valueChanged), this,
-                [this]{ kat_set_range(ui->spinBox_rangeUp->value(), ui->spinBox_rangeDown->value()); });
-
-        connect(ui->spinBox_rangeDown, OVERLOAD_INT(&QSpinBox::valueChanged), this,
-                [this]{ kat_set_range(ui->spinBox_rangeUp->value(), ui->spinBox_rangeDown->value()); });
-
-        connect(ui->spinBox_threshold, OVERLOAD_INT(&QSpinBox::valueChanged), this,
-                [this]{ kat_set_threshold(ui->spinBox_threshold->value()); });
-
-        connect(ui->spinBox_domainSize, OVERLOAD_INT(&QSpinBox::valueChanged), this,
-                [this]{ kat_set_domain_size(ui->spinBox_domainSize->value()); });
-
-        connect(ui->spinBox_matchesReqd, OVERLOAD_INT(&QSpinBox::valueChanged), this,
-                [this]{ kat_set_matches_required(ui->spinBox_matchesReqd->value()); });
-
-        connect(ui->spinBox_stepSize, OVERLOAD_INT(&QSpinBox::valueChanged), this,
-                [this]{ kat_set_step_size(ui->spinBox_stepSize->value()); });
-
-        #undef OVERLOAD_INT
+            if (parameterName == "Scan start") kat_set_range(newValue, ui->parameterGrid_parameters->value("Scan end"));
+            else if (parameterName == "Scan end") kat_set_range(ui->parameterGrid_parameters->value("Scan start"), newValue);
+            else if (parameterName == "Threshold") kat_set_threshold(newValue);
+            else if (parameterName == "Window size") kat_set_domain_size(newValue);
+            else if (parameterName == "Step size") kat_set_step_size(newValue);
+            else if (parameterName == "Matches req'd") kat_set_matches_required(newValue);
+            else k_assert(0, "Unknown parameter identifier");
+        });
 
         connect(ui->checkBox_visualizeRange, &QCheckBox::stateChanged, this,
                 [=]{ update_visualization_options(); });
@@ -141,11 +130,12 @@ AntiTearDialog::AntiTearDialog(QWidget *parent) :
     {
         const anti_tear_options_s defaults = kat_default_settings();
 
-        ui->spinBox_rangeUp->setValue(kpers_value_of(INI_GROUP_ANTI_TEAR, "range_up", defaults.rangeUp).toInt());
-        ui->spinBox_rangeDown->setValue(kpers_value_of(INI_GROUP_ANTI_TEAR, "range_down", defaults.rangeDown).toInt());
-        ui->spinBox_threshold->setValue(kpers_value_of(INI_GROUP_ANTI_TEAR, "threshold", defaults.threshold).toInt());
-        ui->spinBox_matchesReqd->setValue(kpers_value_of(INI_GROUP_ANTI_TEAR, "matches_reqd", defaults.matchesReqd).toInt());
-        ui->spinBox_domainSize->setValue(kpers_value_of(INI_GROUP_ANTI_TEAR, "window_len", defaults.windowLen).toInt());
+        ui->parameterGrid_parameters->set_value("Scan start", kpers_value_of(INI_GROUP_ANTI_TEAR, "range_up", defaults.rangeUp).toInt());
+        ui->parameterGrid_parameters->set_value("Scan end", kpers_value_of(INI_GROUP_ANTI_TEAR, "range_down", defaults.rangeDown).toInt());
+        ui->parameterGrid_parameters->set_value("Threshold", kpers_value_of(INI_GROUP_ANTI_TEAR, "threshold", defaults.threshold).toInt());
+        ui->parameterGrid_parameters->set_value("Window size", kpers_value_of(INI_GROUP_ANTI_TEAR, "window_len", defaults.windowLen).toInt());
+        ui->parameterGrid_parameters->set_value("Step size", kpers_value_of(INI_GROUP_ANTI_TEAR, "step_size", defaults.windowLen).toInt());
+        ui->parameterGrid_parameters->set_value("Matches req'd", kpers_value_of(INI_GROUP_ANTI_TEAR, "matches_reqd", defaults.matchesReqd).toInt());
         ui->checkBox_visualizeRange->setChecked(kpers_value_of(INI_GROUP_ANTI_TEAR, "visualize_range", true).toBool());
         ui->checkBox_visualizeTear->setChecked(kpers_value_of(INI_GROUP_ANTI_TEAR, "visualize_tear", true).toBool());
         this->set_anti_tear_enabled(kpers_value_of(INI_GROUP_ANTI_TEAR, "enabled", kat_is_anti_tear_enabled()).toBool());
@@ -162,11 +152,12 @@ AntiTearDialog::~AntiTearDialog()
     // Save persistent settings.
     {
         kpers_set_value(INI_GROUP_GEOMETRY, "anti_tear", this->size());
-        kpers_set_value(INI_GROUP_ANTI_TEAR, "range_up", ui->spinBox_rangeUp->value());
-        kpers_set_value(INI_GROUP_ANTI_TEAR, "range_down", ui->spinBox_rangeDown->value());
-        kpers_set_value(INI_GROUP_ANTI_TEAR, "threshold", ui->spinBox_threshold->value());
-        kpers_set_value(INI_GROUP_ANTI_TEAR, "window_len", ui->spinBox_domainSize->value());
-        kpers_set_value(INI_GROUP_ANTI_TEAR, "matches_reqd", ui->spinBox_matchesReqd->value());
+        kpers_set_value(INI_GROUP_ANTI_TEAR, "range_up", ui->parameterGrid_parameters->value("Scan start"));
+        kpers_set_value(INI_GROUP_ANTI_TEAR, "range_down", ui->parameterGrid_parameters->value("Scan end"));
+        kpers_set_value(INI_GROUP_ANTI_TEAR, "threshold", ui->parameterGrid_parameters->value("Threshold"));
+        kpers_set_value(INI_GROUP_ANTI_TEAR, "window_len", ui->parameterGrid_parameters->value("Window size"));
+        kpers_set_value(INI_GROUP_ANTI_TEAR, "matches_reqd", ui->parameterGrid_parameters->value("Matches req'd"));
+        kpers_set_value(INI_GROUP_ANTI_TEAR, "step_size", ui->parameterGrid_parameters->value("Step size"));
         kpers_set_value(INI_GROUP_ANTI_TEAR, "visualize_range", ui->checkBox_visualizeRange->isChecked());
         kpers_set_value(INI_GROUP_ANTI_TEAR, "visualize_tear", ui->checkBox_visualizeTear->isChecked());
         kpers_set_value(INI_GROUP_ANTI_TEAR, "direction", 0);
