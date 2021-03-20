8ab648e2c275ae097ca85c2fa5d7a5a8af960300
leikareipa <drilledicecream@gmail.com>
2021-03-21 01:24:57 +0200

    capture/virtual: Allow input resolution to be changed

--- src/capture/capture_api_virtual.cpp
+++ src/capture/capture_api_virtual.cpp
@@ -10,14 +10,79 @@
 #include <chrono>
 #include "common/propagate/app_events.h"
 #include "capture/capture_api_virtual.h"
+#include "common/timer/timer.h"
 
-static unsigned NUM_FRAMES_GENERATED = 0;
+// We'll try to redraw the on-screen test pattern this often.
+static const double TARGET_REFRESH_RATE = 60;
+
+// Keep track of the actual achieved refresh rate.
+static unsigned NUM_FRAMES_PER_SECOND = 0;
+static double CURRENT_REFRESH_RATE = 0;
+static auto REFRESH_RATE_TIMER = std::chrono::steady_clock::now();
+
+static bool CAPTURE_EVENT_FLAGS[(int)capture_event_e::num_enumerators];
+
+static void push_capture_event(const capture_event_e event)
+{
+    CAPTURE_EVENT_FLAGS[(int)event] = true;
+
+    return;
+}
+
+static bool pop_capture_event(const capture_event_e event)
+{
+    const bool oldFlagValue = CAPTURE_EVENT_FLAGS[(int)event];
+
+    CAPTURE_EVENT_FLAGS[(int)event] = false;
+
+    return oldFlagValue;
+}
 
 bool capture_api_virtual_s::initialize(void)
 {
-    this->frameBuffer.r = this->defaultResolution;
+    this->frameBuffer.r = {640, 480, 32};
     this->frameBuffer.pixelFormat = this->defaultPixelFormat;
     this->frameBuffer.pixels.alloc(MAX_NUM_BYTES_IN_CAPTURED_FRAME, "Capture frame buffer (virtual)");
+
+    // Animate the screen's test pattern.
+    kt_timer(std::round(1000 / TARGET_REFRESH_RATE), [this]
+    {
+        static unsigned numFramesGenerated = 0;
+
+        numFramesGenerated++;
+        NUM_FRAMES_PER_SECOND++;
+
+        for (unsigned y = 0; y < this->frameBuffer.r.h; y++)
+        {
+            for (unsigned x = 0; x < this->frameBuffer.r.w; x++)
+            {
+                const unsigned idx = ((x + y * this->frameBuffer.r.w) * (this->frameBuffer.r.bpp / 8));
+
+                this->frameBuffer.pixels[idx + 0] = ((numFramesGenerated + x) % 256);
+                this->frameBuffer.pixels[idx + 1] = ((numFramesGenerated + y) % 256);
+                this->frameBuffer.pixels[idx + 2] = 150;
+                this->frameBuffer.pixels[idx + 3] = 255;
+            }
+        }
+
+        push_capture_event(capture_event_e::new_frame);
+    });
+
+    kt_timer(1000, []
+    {
+        const auto timeNow = std::chrono::steady_clock::now();
+        const auto realMsElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(timeNow - REFRESH_RATE_TIMER).count();
+        const double newRefreshRate = (NUM_FRAMES_PER_SECOND * (1000.0 / realMsElapsed));
+
+        NUM_FRAMES_PER_SECOND = 0;
+        REFRESH_RATE_TIMER = std::chrono::steady_clock::now();
+
+        if (CURRENT_REFRESH_RATE != newRefreshRate)
+        {
+            CURRENT_REFRESH_RATE = newRefreshRate;
+            push_capture_event(capture_event_e::new_video_mode);
+        }
+    });
 
     return true;
 }
@@ -27,6 +92,29 @@ bool capture_api_virtual_s::release(void)
     this->frameBuffer.pixels.release_memory();
 
     return true;
+}
+
+bool capture_api_virtual_s::set_resolution(const resolution_s &r)
+{
+    if ((r.w > this->maxResolution.w) ||
+        (r.w < this->minResolution.w) ||
+        (r.h > this->maxResolution.h) ||
+        (r.h < this->minResolution.h))
+    {
+        return false;
+    }
+
+    this->frameBuffer.r.w = r.w;
+    this->frameBuffer.r.h = r.h;
+
+    push_capture_event(capture_event_e::new_video_mode);
+
+    return true;
+}
+
+refresh_rate_s capture_api_virtual_s::get_refresh_rate() const
+{
+    return refresh_rate_s(CURRENT_REFRESH_RATE);
 }
 
 bool capture_api_virtual_s::set_input_channel(const unsigned idx)
@@ -45,55 +133,16 @@ const captured_frame_s& capture_api_virtual_s::get_frame_buffer(void) const
 
 capture_event_e capture_api_virtual_s::pop_capture_event_queue(void)
 {
-    if (!NUM_FRAMES_GENERATED)
+    if (pop_capture_event(capture_event_e::new_video_mode))
     {
-        NUM_FRAMES_GENERATED++;
-        
         return capture_event_e::new_video_mode;
     }
-    
-    // Normally, the capture card's output rate limits VCS's frame rate;
-    // but for the virtual capture device, we'll need to emulate a delay.
-    static auto startTime = std::chrono::system_clock::now();
-    std::chrono::duration<double> timeDelta = (std::chrono::system_clock::now() - startTime);
-
-    if (std::chrono::duration_cast<std::chrono::milliseconds>(timeDelta).count() <= 16)
+    else if (pop_capture_event(capture_event_e::new_frame))
     {
-        return capture_event_e::none;
-    }
-    else
-    {
-        startTime = std::chrono::system_clock::now();
-
         return capture_event_e::new_frame;
     }
-}
 
-bool capture_api_virtual_s::mark_frame_buffer_as_processed(void)
-{
-    this->animate_frame_buffer();
-
-    return true;
-}
-
-void capture_api_virtual_s::animate_frame_buffer(void)
-{
-    NUM_FRAMES_GENERATED++;
-
-    for (unsigned y = 0; y < this->frameBuffer.r.h; y++)
-    {
-        for (unsigned x = 0; x < this->frameBuffer.r.w; x++)
-        {
-            const unsigned idx = ((x + y * this->frameBuffer.r.w) * (this->frameBuffer.r.bpp / 8));
-
-            this->frameBuffer.pixels[idx + 0] = ((NUM_FRAMES_GENERATED + x) % 256);
-            this->frameBuffer.pixels[idx + 1] = ((NUM_FRAMES_GENERATED + y) % 256);
-            this->frameBuffer.pixels[idx + 2] = 150;
-            this->frameBuffer.pixels[idx + 3] = 255;
-        }
-    }
-
-    return;
+    return capture_event_e::none;
 }
 
 #endif
--- src/capture/capture_api_virtual.h
+++ src/capture/capture_api_virtual.h
@@ -11,6 +11,7 @@
 #define VCS_CAPTURE_CAPTURE_API_VIRTUAL_H
 
 #include "capture/capture_api.h"
+#include "common/globals.h"
 
 struct capture_api_virtual_s : public capture_api_s
 {
@@ -26,14 +27,15 @@ struct capture_api_virtual_s : public capture_api_s
     video_signal_parameters_s get_default_video_signal_parameters(void) const override { return video_signal_parameters_s{}; }
     video_signal_parameters_s get_minimum_video_signal_parameters(void) const override { return video_signal_parameters_s{}; }
     video_signal_parameters_s get_maximum_video_signal_parameters(void) const override { return video_signal_parameters_s{}; }
-    resolution_s get_resolution(void) const override             { return this->defaultResolution; }
-    resolution_s get_minimum_resolution(void) const override     { return this->defaultResolution; }
-    resolution_s get_maximum_resolution(void) const override     { return this->defaultResolution; }
-    refresh_rate_s get_refresh_rate(void) const override         { return refresh_rate_s(0); }
+    bool set_resolution(const resolution_s &r) override;
+    resolution_s get_resolution(void) const override             { return this->frameBuffer.r; }
+    resolution_s get_minimum_resolution(void) const override     { return this->minResolution; }
+    resolution_s get_maximum_resolution(void) const override     { return this->maxResolution; }
+    refresh_rate_s get_refresh_rate(void) const override;
     uint get_missed_frames_count(void) const override            { return 0; }
     uint get_input_channel_idx(void) const override              { return this->inputChannelIdx; }
     bool set_input_channel(const unsigned idx) override;
-    uint get_color_depth(void) const override                    { return (unsigned)this->defaultResolution.bpp; }
+    uint get_color_depth(void) const override                    { return (unsigned)this->frameBuffer.r.bpp; }
     bool is_capturing(void) const override                       { return false; }
     bool has_invalid_signal(void) const override                 { return false; }
     bool has_invalid_device(void) const override                 { return false; }
@@ -41,7 +43,6 @@ struct capture_api_virtual_s : public capture_api_s
     capture_pixel_format_e get_pixel_format(void) const override { return this->defaultPixelFormat; }
     capture_event_e pop_capture_event_queue(void) override;
     const captured_frame_s& get_frame_buffer(void) const override;
-    bool mark_frame_buffer_as_processed(void) override;
 
     bool device_supports_component_capture(void) const override  { return true; }
     bool device_supports_composite_capture(void) const override  { return true; }
@@ -53,13 +54,9 @@ struct capture_api_virtual_s : public capture_api_s
     bool device_supports_yuv(void) const override                { return true; }
 
 private:
-    const resolution_s defaultResolution = resolution_s{640, 480, 32};
-
+    const resolution_s maxResolution = resolution_s{MAX_CAPTURE_WIDTH, MAX_CAPTURE_HEIGHT, 32};
+    const resolution_s minResolution = resolution_s{320, 200, 32};
     const capture_pixel_format_e defaultPixelFormat = capture_pixel_format_e::rgb_888;
-
-    // We don't do any actual capturing, so draw an animating pattern in the
-    // frame buffer, instead.
-    void animate_frame_buffer(void);
 
     captured_frame_s frameBuffer;
 
