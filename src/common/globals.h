90d4604b5c16fd8c691bb24bc93b11bcf9bb9f9d
leikareipa <drilledicecream@gmail.com>
2021-03-25 06:15:51 +0200

    Bump version number

--- src/common/globals.h
+++ src/common/globals.h
@@ -18,7 +18,7 @@
 #include <cassert>
 
 const char PROGRAM_NAME[] = "VCS";
-const char PROGRAM_VERSION_STRING[] = "2.3.1";
+const char PROGRAM_VERSION_STRING[] = "2.3.2";
 
 // The minimum and maximum resolution we can output frames in.
 const uint MIN_OUTPUT_WIDTH = 200;
