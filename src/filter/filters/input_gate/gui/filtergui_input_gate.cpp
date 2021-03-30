a66269eb68e25de7658107eeef2c8089918aacef
leikareipa <drilledicecream@gmail.com>
2021-03-30 05:02:09 +0300

    filter: Stack input & output gates' GUI controls horizontally

--- src/filter/filters/input_gate/gui/filtergui_input_gate.cpp
+++ src/filter/filters/input_gate/gui/filtergui_input_gate.cpp
@@ -13,24 +13,21 @@ filtergui_input_gate_c::filtergui_input_gate_c(filter_c *const filter)
 {
     {
         auto *const width = new filtergui_spinbox_s;
-
         width->get_value = [=]{return filter->parameter(filter_input_gate_c::PARAM_WIDTH);};
         width->set_value = [=](const double value){filter->set_parameter(filter_input_gate_c::PARAM_WIDTH, value);};
         width->minValue = 0;
         width->maxValue = MAX_CAPTURE_WIDTH;
 
-        this->guiFields.push_back({"Width", {width}});
-    }
+        auto *const separator = new filtergui_label_s;
+        separator->text = "\u00d7";
 
-    {
         auto *const height = new filtergui_spinbox_s;
-
         height->get_value = [=]{return filter->parameter(filter_input_gate_c::PARAM_HEIGHT);};
         height->set_value = [=](const double value){filter->set_parameter(filter_input_gate_c::PARAM_HEIGHT, value);};
         height->minValue = 0;
         height->maxValue = MAX_CAPTURE_HEIGHT;
 
-        this->guiFields.push_back({"Height", {height}});
+        this->guiFields.push_back({"", {width, separator, height}});
     }
 
     return;
