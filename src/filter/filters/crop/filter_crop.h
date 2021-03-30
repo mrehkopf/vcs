dd511f6d4ba04e9c63b4ab9aa6068da03ba9742b
leikareipa <drilledicecream@gmail.com>
2021-03-30 04:21:24 +0300

    filter: Put crop filter under the Reduce category

--- src/filter/filters/crop/filter_crop.h
+++ src/filter/filters/crop/filter_crop.h
@@ -36,7 +36,7 @@ public:
     std::string uuid(void) const override { return "2448cf4a-112d-4d70-9fc1-b3e9176b6684"; }
     std::string name(void) const override { return "Crop"; }
     filter_type_e type(void) const override { return filter_type_e::crop; }
-    filter_category_e category(void) const override { return filter_category_e::distort; }
+    filter_category_e category(void) const override { return filter_category_e::reduce; }
 
 private:
 };
