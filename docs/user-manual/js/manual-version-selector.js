4ce829be6ba0b425168462e8f03d2fe8ab8633fb
leikareipa <drilledicecream@gmail.com>
2022-10-31 20:25:31 +0200

    docs/user-manual: Add ability to select manual's version
    The base version of the manual is now targeted at the VCS repo's
    master branch, including documentation for features that aren't yet
    available in a binary release.
    
    Outside of the repo, I host snapshots of the manual for the various
    point releases of VCS, which this new version selector widget lets the
    user switch between.

---
+++ docs/user-manual/js/manual-version-selector.js
@@ -0,0 +1,128 @@
+/*
+ * 2022 Tarpeeksi Hyvae Soft
+ *
+ * Software: Version selector widget for VCS User's Manual
+ * 
+ * 
+ * Intended for use as a custom widget in VCS's dokki-based user's manual.
+ * 
+ * The widget presents the user a selection box for choosing which version of the
+ * manual to view.
+ *
+ */
+
+const manualVersionSelector = {
+    $tag: "manual-version-selector",
+    data() {
+        return {
+            inlineSvgFillColor: "currentColor",
+        };
+    },
+    computed: {
+        supported_manual_versions() {
+            return [
+                "master",
+                "2.5.1",
+                "2.5.0",
+            ];
+        },
+        current_manual_version() {
+            return window.VCS_MANUAL_VERSION;
+        },
+    },
+    methods: {
+        update_inline_svg_fill_color() {
+            this.inlineSvgFillColor = (
+                getComputedStyle(document.body)
+                .getPropertyValue("--dokkiCSS-page-primary-fg-color")
+                .replace("#", "%23")
+            );
+        },
+        on_select(event) {
+            const newUrl = (
+                (event.target.value === "master")
+                ? "/vcs/docs/user-manual/"
+                : `/vcs/docs/user-manual/${event.target.value}/`
+            );
+
+            window.history.replaceState(null, "", newUrl);
+            window.location.reload();
+        },
+        is_supported_manual_version(version = "") {
+            return this.supported_manual_versions.includes(version);
+        },
+    },
+    created() {
+        this.update_inline_svg_fill_color();
+
+        new MutationObserver((mutations)=>{
+            if (mutations.some(m=>m.attributeName === "data-dokki-theme")) {
+                this.update_inline_svg_fill_color();
+            }
+        }).observe(document.body, {
+            attributes: true, childList: false, characterData: false
+        });
+    },
+    template: `
+    <dokki-user-widget
+        v-if="is_supported_manual_version(current_manual_version)"
+        class="manual-version-selector"
+        title="Select a different version of VCS"
+    >
+        <i class="widget-icon fas fa-book"></i>
+        <select @change="on_select">
+            <option
+                v-for="version in supported_manual_versions"
+                :selected="version === current_manual_version"
+            >
+                {{version}}
+            </option>
+        </select>
+    </dokki-user-widget>
+
+    <component is="style" type="text/css">
+        .dokki-user-widget.manual-version-selector i
+        {
+            margin-right: 0.25em !important;
+        }
+
+        .dokki-user-widget.manual-version-selector
+        {
+            color: var(--dokkiCSS-page-primary-fg-color);
+            font-family: var(--dokkiCSS-regular-font-family);
+            font-size: var(--dokkiCSS-regular-font-size);
+        }
+
+        .dokki-user-widget.manual-version-selector select
+        {
+            border: none;
+            background-color: var(--dokkiCSS-page-primary-bg-color);
+            outline: none;
+            border-color: var(--dokkiCSS-page-primary-line-color);
+            font-family: inherit;
+            font-size: inherit;
+            color: inherit;
+            appearance: none;
+            padding: 0 0.25em;
+            padding-right: 1.25em;
+            background-image: url("
+                data:image/svg+xml;utf8,
+                &lt;svg fill='{{inlineSvgFillColor}}' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'&gt;
+                &lt;path d='M7 10l5 5 5-5z'/&gt;&lt;path d='M0 0h24v24H0z' fill='none'/&gt;
+                &lt;/svg&gt;
+            ");
+            background-repeat: no-repeat;
+            background-position: top 0 right 0;
+            background-size: 1.25em auto;
+        }
+
+        .dokki-user-widget.manual-version-selector select:hover
+        {
+            cursor: pointer;
+        }
+    </component>
+    `,
+};
+
+window.dokkiUserComponents = (window.dokkiUserComponents || []);
+window.dokkiUserComponents.push(manualVersionSelector);
