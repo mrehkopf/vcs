dda44a8e85317e0ac9f12dabe83ceae91639ba35
leikareipa <drilledicecream@gmail.com>
2023-09-25 07:27:43 +0300

    ui/histogram: Remove option to set target color channel
    I don't think it's a very useful feature, and takes up space.

--- src/display/qt/windows/ControlPanel/Output/Histogram.cpp
+++ src/display/qt/windows/ControlPanel/Output/Histogram.cpp
@@ -16,33 +16,17 @@
 #include "ui_Histogram.h"
 #include <opencv2/imgproc/imgproc.hpp>
 
-// Maps strings from a channel selector in the UI to internal channel enumerators.
-static const std::unordered_map<std::string, ColorHistogram::channels> CHANNELS = {
-    {"RGB", ColorHistogram::channels::rgb},
-    {"Red", ColorHistogram::channels::red},
-    {"Green", ColorHistogram::channels::green},
-    {"Blue", ColorHistogram::channels::blue},
-};
-
 control_panel::output::Histogram::Histogram(QWidget *parent) :
     DialogFragment(parent),
     ui(new Ui::Histogram)
 {
     this->ui->setupUi(this);
 
-    connect(this->ui->channels, &QComboBox::currentTextChanged, this, [this](const QString &channels)
-    {
-        if (this->isVisible())
-        {
-            this->ui->histogram->refresh(ks_scaler_frame_buffer(), CHANNELS.at(channels.toStdString()));
-        }
-    });
-
     ev_new_output_image.listen([this](const image_s &image)
     {
         if (this->isVisible())
         {
-            this->ui->histogram->refresh(image, CHANNELS.at(this->ui->channels->currentText().toStdString()));
+            this->ui->histogram->refresh(image);
         }
     });
 
@@ -54,7 +38,7 @@ control_panel::output::Histogram::Histogram(QWidget *parent) :
         }
     });
 
-    this->ui->histogram->refresh(ks_scaler_frame_buffer(), CHANNELS.at(this->ui->channels->currentText().toStdString()));
+    this->ui->histogram->refresh(ks_scaler_frame_buffer());
 
     return;
 }
