1276c023abb027e8a83b7667010044c6ce7926a7
leikareipa <drilledicecream@gmail.com>
2023-09-28 06:20:51 +0300

    ui/filter-graph: Fix gates' on/off toggles not being correctly initialized

--- src/filter/filters/input_gate/gui/filtergui_input_gate.cpp
+++ src/filter/filters/input_gate/gui/filtergui_input_gate.cpp
@@ -14,6 +14,7 @@ filtergui_input_gate_c::filtergui_input_gate_c(abstract_filter_c *const filter)
 {
     {
         auto *const width = new abstract_gui::spinner;
+        width->isInitiallyEnabled = filter->parameter(filter_input_gate_c::PARAM_IS_WIDTH_ENABLED);
         width->get_value = [=]{return filter->parameter(filter_input_gate_c::PARAM_WIDTH);};
         width->set_value = [=](const int value){filter->set_parameter(filter_input_gate_c::PARAM_WIDTH, value);};
         width->minValue = 0;
@@ -31,6 +32,7 @@ filtergui_input_gate_c::filtergui_input_gate_c(abstract_filter_c *const filter)
 
     {
         auto *const height = new abstract_gui::spinner;
+        height->isInitiallyEnabled = filter->parameter(filter_input_gate_c::PARAM_IS_HEIGHT_ENABLED);
         height->get_value = [=]{return filter->parameter(filter_input_gate_c::PARAM_HEIGHT);};
         height->set_value = [=](const int value){filter->set_parameter(filter_input_gate_c::PARAM_HEIGHT, value);};
         height->minValue = 0;
